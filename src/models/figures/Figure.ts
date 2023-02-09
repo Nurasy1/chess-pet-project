94ffa4465295ae3c666f905c8314928a093f7258
100ping <princenurik04@mail.ru>
2023-02-09 23:15:38 +0600

    Figures

--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,3 +1,30 @@
+import {Colors} from "./Colors";
+import {Cell} from "./Cell";
+import logo from "../../assets/black-king.png"
+
+export enum FigureNames {
+    FIGURE ="",
+    FIGURE ="",
+    FIGURE ="",
+    FIGURE ="",
+    FIGURE ="",
+    FIGURE ="",
+    FIGURE ="",
+}
+
 export class Figure{
+color : Colors;
+logo : typeof logo | null;
+cell : Cell;
+name : FigureNames;
+id : number;
+
+constructor(color : Colors , cell : Cell) {
+    this.color = color;
+    this.cell = cell;
+    this.cell.figure = this;
+    this.logo = null;
+    this.name = FigureNames.FIGURE
+}
 
 }
