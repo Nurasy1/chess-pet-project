5369d55a62a9e71474f513db62cadc3ff8e5547c
100ping <princenurik04@mail.ru>
2023-02-13 01:15:26 +0600

    пешка и конь

--- src/models/figures/Pawn.ts
+++ src/models/figures/Pawn.ts
@@ -4,15 +4,39 @@ import {Cell} from "../Cell";
 import blacklogo from "../../assets/black-pawn.png";
 import whitelogo from "../../assets/white-pawn.png";
 
-export class Pawn extends Figure{
+export class Pawn extends Figure {
+
+    isFirsStep: boolean = true;
+
     constructor(color: Colors, cell: Cell) {
         super(color, cell);
         this.logo = color === Colors.BLACK ? blacklogo : whitelogo;
         this.name = FigureNames.PAWN;
     }
+
     canMove(target: Cell): boolean {
         if (!super.canMove(target))
             return false;
-        return true
+        const direction = this.cell.figure?.color === Colors.BLACK ? 1 : -1
+        const firstStepDirection = this.cell.figure?.color === Colors.BLACK ? 2 : -2
+
+
+
+         if ((target.y === this.cell.y + direction || this.isFirsStep
+                 && (target.y === this.cell.y + firstStepDirection))
+             && target.x === this.cell.x
+             && this.cell.board.getCell(target.x, target.y).isEmpty()) {
+             return true;
+         }
+
+
+
+        return false;
+    }
+
+
+    moveFigure(target: Cell) {
+        super.moveFigure(target);
+        this.isFirsStep = false;
     }
 }
